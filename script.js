e80d95135a418d4ba9b4d41487f73c0e41f4fccd
Ekaterina Skirdina <katyonok.ya17@gmail.com>
2024-02-24 18:14:12 +0300

    The Articles & News block was created with the help of Vue.js, where the layout data will be stored in an array of objects and output to the page using a v-for loop.

---
+++ script.js
@@ -0,0 +1,51 @@
+const blog = {
+    data() {
+        return {
+            items: [
+                {
+                    id: 1,
+                    image: "img/blogCard1.jpg",
+                    tag: "Kitchan Design",
+                    title: "Let's Get Solution For Building Construction Work",
+                    date: "26 December,2022",
+                },
+                {
+                    id: 2,
+                    image: "img/blogCard2.jpg",
+                    tag: "Living Design",
+                    title: "Low Cost Latest Invented Interior Designing \n Ideas",
+                    date: "22 December,2022 ",
+                },
+                {
+                    id: 3,
+                    image: "img/blogCard3.jpg",
+                    tag: "Interior Design",
+                    title: "Best For Any Office & Business Interior                  Solution",
+                    date: "25 December,2022 ",
+                },
+                {
+                    id: 4,
+                    image: "img/blogCard4.jpg",
+                    tag: "Kitchan Design",
+                    title: "Let's Get Solution For Building Construction Work",
+                    date: "26 December,2022",
+                },
+                {
+                    id: 5,
+                    image: "img/blogCard5.jpg",
+                    tag: "Living Design",
+                    title: "Low Cost Latest Invented Interior Designing \n Ideas",
+                    date: "22 December,2022 ",
+                },
+                {
+                    id: 6,
+                    image: "img/blogCard6.jpg",
+                    tag: "Interior Design",
+                    title: "Best For Any Office & Business Interior                  Solution",
+                    date: "25 December,2022 ",
+                },
+            ]
+        }
+    },
+}
+Vue.createApp(blog).mount("#blog");
